ae29b42fcfa18b9633f636098c22135780c964f7
MohammedShaibani <sonngohun@hotmail.ca>
2021-10-12 13:42:46 -0400

    adding job components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Jobs from './Jobs'
+
+const mockJobs = [
+  {title: 'SWE Job 1', company: 'google'},
+  {title: 'SWE Job 1', company: 'facebook'},
+  {title: 'SWE Job 1', company: 'apple'},
+  {title: 'SWE Job 1', company: 'microsoft'}
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jobs jobs= {mockJobs} />
     </div>
   );
 }
